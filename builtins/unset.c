beb5a79725da9e93de2e78589f7be8eace2ef5a0
rnittaBadCommit <ryushironitta@yahoo.co.jp>
2021-02-11 22:37:34 +0900

    modify unset

--- builtins/unset.c
+++ builtins/unset.c
@@ -6,18 +6,52 @@
 /*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/10 13:31:08 by marvin            #+#    #+#             */
-/*   Updated: 2021/02/10 13:31:10 by marvin           ###   ########.fr       */
+/*   Updated: 2021/02/11 22:37:22 by marvin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-int	ft_unset(char *argv[], t_arg_main *arg_main)
+void	unset_err(int err, char *cmd)
 {
-	while (argv[1])
+	if (err == BAD_ARGNAME)
 	{
-		arg_delete(arg_main, argv[1]);
-		argv++;
+		ft_putstr_fd("bash: unset: `", 2);
+		ft_putstr_fd(cmd, 2);
+		ft_putstr_fd("\': not a valid identifier\n", 2);
+	}
+}
+
+int		unset_name_check(char *cmd)
+{
+	if (!ft_isalpha(*cmd) && *cmd != '_')
+		return (1);
+	if (*cmd == '\0')
+		return (1);
+	while (*cmd)
+	{
+		if (!ft_isalnum(*cmd) && *cmd != '_')
+			return (1);
+		cmd++;
 	}
 	return (0);
+}
+
+int	ft_unset(char *argv[], t_arg_main *arg_main)
+{
+	int	ret;
+
+	ret = 0;
+	while (argv[1])
+	{
+		if (!unset_name_check(argv[1]))
+		{
+			unset_err(BAD_ARGNAME, argv[1]);
+			ret = 1;
+		}
+		else
+			arg_delete(arg_main, argv[1]);
+		argv++;
+	}
+	return (ret);
 }
